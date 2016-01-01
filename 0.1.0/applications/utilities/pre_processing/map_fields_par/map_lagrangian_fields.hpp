ad4bce4b84ee7a1bf7c8d239928c25b80e5082da
Alexey Matveichev <alexey.matveichev@ymail.com>
2016-01-01 21:34:29 +0100

    Update: mousse-map-fields-par

---
+++ 0.1.0/applications/utilities/pre_processing/map_fields_par/map_lagrangian_fields.hpp
@@ -0,0 +1,123 @@
+// mousse: CFD toolbox
+// Copyright (C) 2011-2015 OpenFOAM Foundation
+// Copyright (C) 2016 mousse project
+
+#ifndef map_lagrangian_fields_hpp_
+#define map_lagrangian_fields_hpp_
+#include "cloud.hpp"
+#include "geometric_field.hpp"
+#include "mesh_to_mesh.hpp"
+#include "ioobject_list.hpp"
+#include "compact_io_field.hpp"
+namespace mousse
+{
+//- Gets the indices of (source)particles that have been appended to the
+//  target cloud and maps the lagrangian fields accordingly.
+template<class Type>
+void MapLagrangianFields
+(
+  const string& cloudName,
+  const IOobjectList& objects,
+  const polyMesh& meshTarget,
+  const labelList& addParticles
+)
+{
+  {
+    IOobjectList fields = objects.lookupClass(IOField<Type>::typeName);
+    forAllIter(IOobjectList, fields, fieldIter)
+    {
+      const word& fieldName = fieldIter()->name();
+      Info<< "    mapping lagrangian field " << fieldName << endl;
+      // Read field (does not need mesh)
+      IOField<Type> fieldSource(*fieldIter());
+      // Map
+      IOField<Type> fieldTarget
+      (
+        IOobject
+        (
+          fieldName,
+          meshTarget.time().timeName(),
+          cloud::prefix/cloudName,
+          meshTarget,
+          IOobject::NO_READ,
+          IOobject::NO_WRITE,
+          false
+        ),
+        addParticles.size()
+      );
+      forAll(addParticles, i)
+      {
+        fieldTarget[i] = fieldSource[addParticles[i]];
+      }
+      // Write field
+      fieldTarget.write();
+    }
+  }
+  {
+    IOobjectList fieldFields =
+      objects.lookupClass(IOField<Field<Type> >::typeName);
+    forAllIter(IOobjectList, fieldFields, fieldIter)
+    {
+      const word& fieldName = fieldIter()->name();
+      Info<< "    mapping lagrangian fieldField " << fieldName << endl;
+      // Read field (does not need mesh)
+      IOField<Field<Type> > fieldSource(*fieldIter());
+      // Map - use CompactIOField to automatically write in
+      // compact form for binary format.
+      CompactIOField<Field<Type>, Type> fieldTarget
+      (
+        IOobject
+        (
+          fieldName,
+          meshTarget.time().timeName(),
+          cloud::prefix/cloudName,
+          meshTarget,
+          IOobject::NO_READ,
+          IOobject::NO_WRITE,
+          false
+        ),
+        addParticles.size()
+      );
+      forAll(addParticles, i)
+      {
+        fieldTarget[i] = fieldSource[addParticles[i]];
+      }
+      // Write field
+      fieldTarget.write();
+    }
+  }
+  {
+    IOobjectList fieldFields =
+      objects.lookupClass(CompactIOField<Field<Type>, Type>::typeName);
+    forAllIter(IOobjectList, fieldFields, fieldIter)
+    {
+      Info<< "    mapping lagrangian fieldField "
+        << fieldIter()->name() << endl;
+      // Read field (does not need mesh)
+      CompactIOField<Field<Type>, Type> fieldSource(*fieldIter());
+      // Map
+      CompactIOField<Field<Type>, Type> fieldTarget
+      (
+        IOobject
+        (
+          fieldIter()->name(),
+          meshTarget.time().timeName(),
+          cloud::prefix/cloudName,
+          meshTarget,
+          IOobject::NO_READ,
+          IOobject::NO_WRITE,
+          false
+        ),
+        addParticles.size()
+      );
+      forAll(addParticles, i)
+      {
+        fieldTarget[i] = fieldSource[addParticles[i]];
+      }
+      // Write field
+      fieldTarget.write();
+    }
+  }
+}
+}  // namespace mousse
+#endif
---
+++ 0.1.0/applications/utilities/pre_processing/map_fields_par/map_meshes.hpp
@@ -0,0 +1,88 @@
+// mousse: CFD toolbox
+// Copyright (C) 2011-2015 OpenFOAM Foundation
+// Copyright (C) 2016 mousse project
+
+#ifndef map_meshes_hpp_
+#define map_meshes_hpp_
+#include "map_vol_fields.hpp"
+#include "map_lagrangian.hpp"
+#include "un_mapped.hpp"
+#include "point_mesh.hpp"
+namespace mousse
+{
+template<template<class> class CombineOp>
+void MapMesh
+(
+  const meshToMesh& interp,
+  const HashSet<word>& selectedFields,
+  const bool noLagrangian
+)
+{
+  {
+    const polyMesh& meshSource = interp.srcRegion();
+    // Search for list of objects for this time
+    IOobjectList objects(meshSource, meshSource.time().timeName());
+    // Map volFields
+    // ~~~~~~~~~~~~~
+    MapVolFields<scalar>
+    (
+      objects,
+      selectedFields,
+      interp,
+      CombineOp<scalar>()
+    );
+    MapVolFields<vector>
+    (
+      objects,
+      selectedFields,
+      interp,
+      CombineOp<vector>()
+    );
+    MapVolFields<sphericalTensor>
+    (
+      objects,
+      selectedFields,
+      interp,
+      CombineOp<sphericalTensor>()
+    );
+    MapVolFields<symmTensor>
+    (
+      objects,
+      selectedFields,
+      interp,
+      CombineOp<symmTensor>()
+    );
+    MapVolFields<tensor>
+    (
+      objects,
+      selectedFields,
+      interp,
+      CombineOp<tensor>()
+    );
+  }
+  {
+    const polyMesh& meshTarget = interp.tgtRegion();
+    // Search for list of target objects for this time
+    IOobjectList objects(meshTarget, meshTarget.time().timeName());
+    // Mark surfaceFields as unmapped
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    UnMapped<surfaceScalarField>(objects);
+    UnMapped<surfaceVectorField>(objects);
+    UnMapped<surfaceSphericalTensorField>(objects);
+    UnMapped<surfaceSymmTensorField>(objects);
+    UnMapped<surfaceTensorField>(objects);
+    // Mark pointFields as unmapped
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    UnMapped<pointScalarField>(objects);
+    UnMapped<pointVectorField>(objects);
+    UnMapped<pointSphericalTensorField>(objects);
+    UnMapped<pointSymmTensorField>(objects);
+    UnMapped<pointTensorField>(objects);
+  }
+  if (!noLagrangian)
+  {
+    mapLagrangian(interp);
+  }
+}
+}  // namespace mousse
+#endif
