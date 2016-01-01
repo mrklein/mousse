e1e2abf76d244071f7d69795e9830c322c058d4d
Alexey Matveichev <alexey.matveichev@ymail.com>
2016-01-01 21:48:00 +0100

    Update: mousse-probe-locations

---
+++ 0.1.0/applications/utilities/post_processing/sampling/probe_locations/probe_locations.cpp
@@ -0,0 +1,44 @@
+// mousse: CFD toolbox
+// Copyright (C) 2011-2015 OpenFOAM Foundation
+// Copyright (C) 2016 mousse project
+
+#include "arg_list.hpp"
+#include "time_selector.hpp"
+#include "io_probes.hpp"
+using namespace mousse;
+int main(int argc, char *argv[])
+{
+  timeSelector::addOptions();
+  #include "add_region_option.hpp"
+  #include "set_root_case.hpp"
+  #include "create_time.hpp"
+  instantList timeDirs = timeSelector::select0(runTime, args);
+  #include "create_named_mesh.hpp"
+  IOprobes sniff
+  (
+    probes::typeName,
+    mesh,
+    word("probesDict"), // force the use of the system directory
+    IOobject::MUST_READ,
+    true
+  );
+  forAll(timeDirs, timeI)
+  {
+    runTime.setTime(timeDirs[timeI], timeI);
+    Info<< "Time = " << runTime.timeName() << endl;
+    // Handle geometry/topology changes
+    polyMesh::readUpdateState state = mesh.readUpdate();
+    if
+    (
+      state == polyMesh::POINTS_MOVED
+    || state == polyMesh::TOPO_CHANGE
+    )
+    {
+      sniff.read();
+    }
+    sniff.write();
+    Info<< endl;
+  }
+  Info<< "End\n" << endl;
+  return 0;
+}
